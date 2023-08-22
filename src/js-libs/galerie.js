4c4455cf766a6ffafbb2ff0621dad0e858a3f966
JoeWebDev70 <joellemoureu@gmail.com>
2023-08-22 13:07:23 +0200

    ci: JSON
    create JSON file for loading images in gallery and test the response

---
+++ src/js-libs/galerie.js
@@ -0,0 +1,12 @@
+const mainGallery = document.querySelector("#main_galerie");
+
+if (mainGallery != null) {
+    fetch("../ressources/loading_img.json").then(
+        reponse => reponse.json()).then(
+            data => {
+                data.forEach(element => {
+                    console.log(element);
+                });
+            }
+        )
+}
--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import "./style.scss";
 import "./js-libs/header.js";
 import "./js-libs/footer.js";
+import "./js-libs/galerie.js";
 import "./js-libs/transition.js";
 
